4b47a4479ae12fa28fae67afb328224df00e60ea
AlbertoAlfredo <raioalfredo@gmail.com>
2021-05-06 11:50:05 -0300

    muahahahaha

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const TituloPagina = styled.h1`
   min-height: 7rem;
   text-align: center;
 `;
-  const Header = styled.div `
+const Header = styled.div`
   width: 100%;
   min-height: 527px;
   justify-content: center;
@@ -40,7 +40,7 @@ max-height: 500px;
 object-fit: cover;
 `
 
-const NavigationBar = styled.nav `
+const NavigationBar = styled.nav`
 display: inline-flex;
 flex-direction: row;
 align-items: center;
@@ -90,6 +90,45 @@ const SubtituloCard = styled.h3`
   font-size: 2.5rem;
   font-weight: 400;
 `
+const TextCard = styled.p`
+
+`
+const Coluna = styled.div`
+  background-color: ${props => props.backgroundColor};
+  padding-top: ${props => props.altura};
+  padding-bottom: ${props => props.altura};
+`
+const ContainerColuna = styled(Container)`
+  display: flex;
+  flex-direction: row;
+`
+const ColunaMenor = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 35%;
+`
+
+const ColunaMaior = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 60%;
+`
+const TituloColuna = styled(TituloCard)`
+  color: ${props => props.textColor};
+`
+const SubtituloColuna = styled(SubtituloCard)`
+  color: ${props => props.textColor};
+`
+const TextColuna = styled(TextCard)`
+  color:${props => props.textColor};
+`
+const ColunaLink = styled(NavLink)`
+color: ${props => props.textColor};
+padding: 25px 15px 25px 15px;
+margin: 0;
+font-family: "Roboto", "Open Sans", "Helvetica Neue", "Helvetica", "sans-serif";
+font-size: 20px;
+`;
 
 const publico = process.env.PUBLIC_URL
 
@@ -107,18 +146,18 @@ function App() {
         <Header>
           <TituloPagina>Pousada Recanto do Sossego</TituloPagina>
           <Banner src={banner} />
-          <br/>
+          <br />
           <NavigationBar>
 
-                <NavigationLink className="NavLink" exact="true" activeClassName="NavLinkSelected" to="/" >A Pousada</NavigationLink>
+            <NavigationLink className="NavLink" exact="true" activeClassName="NavLinkSelected" to="/" >A Pousada</NavigationLink>
 
-                <NavigationLink className="NavLink" activeClassName="NavLinkSelected" to="/sobre">Sobre Nós</NavigationLink>
+            <NavigationLink className="NavLink" activeClassName="NavLinkSelected" to="/sobre">Sobre Nós</NavigationLink>
 
-                <NavigationLink className="NavLink" activeClassName="NavLinkSelected" to="/instalacoes">Instalações</NavigationLink>
+            <NavigationLink className="NavLink" activeClassName="NavLinkSelected" to="/instalacoes">Instalações</NavigationLink>
 
-                <NavigationLink className="NavLink" activeClassName="NavLinkSelected" to="/localizacao">Localização</NavigationLink>
+            <NavigationLink className="NavLink" activeClassName="NavLinkSelected" to="/localizacao">Localização</NavigationLink>
 
-                <NavigationLink className="NavLink" activeClassName="NavLinkSelected" to="/contato">Contato</NavigationLink>
+            <NavigationLink className="NavLink" activeClassName="NavLinkSelected" to="/contato">Contato</NavigationLink>
 
           </NavigationBar>
         </Header>
@@ -148,27 +187,75 @@ export default App;
 function Home() {
   return (
     <>
-    <Container>
+      <Container>
+        <Espacador />
+        <ContainerCard>
+          <Card>
+            <img src={publico + '/img/7.jpg'} alt="" />
+            <TituloCard>SIMPLICIDADE E CONFORTO</TituloCard>
+            <SubtituloCard>TRANQUILIDADE PARA O CORPO E A MENTE</SubtituloCard>
+            <TextCard>Longe do barulho e stress da cidade grande, aqui, podemos aproveitar uma estadia relaxante, com a simplicidade  do interior e o conforto necessário para nosso descanso.</TextCard>
+          </Card>
+          <Espacador />
+          <Card>
+            <img src={publico + '/img/7.jpg'} alt="" />
+            <TituloCard>SIMPLICIDADE E CONFORTO</TituloCard>
+            <SubtituloCard>TRANQUILIDADE PARA O CORPO E A MENTE</SubtituloCard>
+            <TextCard>Longe do barulho e stress da cidade grande, aqui, podemos aproveitar uma estadia relaxante, com a simplicidade  do interior e o conforto necessário para nosso descanso.</TextCard>
+          </Card>
+        </ContainerCard>
+      </Container>
       <Espacador/>
-      <ContainerCard>
-        <Card>
-          <img src={publico + '/img/7.jpg'}></img>
-          <TituloCard>SIMPLICIDADE E CONFORTO</TituloCard>
-          <SubtituloCard>TRANQUILIDADE PARA O CORPO E A MENTE</SubtituloCard>
-          <p>Longe do barulho e stress da cidade grande, aqui, podemos aproveitar uma estadia relaxante, com a simplicidade  do interior e o conforto necessário para nosso descanso.</p>
-        </Card> 
-        <Espacador/>
-        <Card>
-          <img src={publico + '/img/7.jpg'}></img>
-          <TituloCard>SIMPLICIDADE E CONFORTO</TituloCard>
-          <SubtituloCard>TRANQUILIDADE PARA O CORPO E A MENTE</SubtituloCard>
-          <p>Longe do barulho e stress da cidade grande, aqui, podemos aproveitar uma estadia relaxante, com a simplicidade  do interior e o conforto necessário para nosso descanso.</p>
-        </Card>
-        
-        
-      </ContainerCard>
-    </Container>
-      <Espacador/>
+      <Coluna backgroundColor="#000" altura="100px">
+        <ContainerColuna>
+          <ColunaMenor>
+            <TituloColuna textColor="#fff">
+              Como Começou a Pousada?
+          </TituloColuna>
+            <SubtituloColuna textColor="#fff">
+              Recanto do Sossego Serra do Cipó
+          </SubtituloColuna>
+            <TextColuna textColor="#fff">
+              A pousada está em atividade desde 1999. Seu proprietário, Narcizo Raimundo da Silva, morador no local, viu em seu sítio um local relaxante e agradável para construí-la de forma a compartilhar isso com outras pessoas. A pusada, recebe vários hóspedes de diversos lugares que vêm em busca de sossego,  descanso e lazer.
+          </TextColuna>
+            <ColunaLink to="/sobre">Leia Mais</ColunaLink>
+            <Espacador/>
+            <TituloColuna textColor="#fff">
+              Mais que uma Pousada
+            </TituloColuna>
+            <SubtituloColuna textColor="#fff">
+              Aqui, Fazemos Amigos
+            </SubtituloColuna>
+            <TextColuna textColor="#fff">
+            As pessoas que passam por aqui são mais  que hóspedes, são amigos. Já os que estão por vir são os amigos que ainda não conhecemos.
+            </TextColuna>
+            <ColunaLink to="/sobre">Leia Mais</ColunaLink>
+          </ColunaMenor>
+          <Espacador/>
+          <ColunaMaior>
+            <iframe title="GoogleMaps" src="https://www.google.com/maps/embed?pb=!1m22!1m8!1m3!1d29793.063134277276!2d-43.64654747802237!3d-19.39795712076952!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d-19.3754968!2d-43.6611626!4m5!1s0xa5e8ac82f05065%3A0xdb581dfb8008d5e6!2sUnnamed%20Rd%2C%20Jaboticatubas%20-%20MG%2C%2035830-000%2C%20Brasil!3m2!1d-19.403034299999998!2d-43.632829099999995!5e1!3m2!1spt-BR!2sbr!4v1620310519277!5m2!1spt-BR!2sbr" width="600" height="450" style={{ border: 0 }} allowfullscreen="" loading="lazy"></iframe>
+          </ColunaMaior>
+        </ContainerColuna>
+      </Coluna>
+      <Coluna altura="100px">
+        <ContainerColuna>
+          <ColunaMaior>
+          <div class="zoom"><img src={publico + "/img/rede.jpg"}/></div>
+            </ColunaMaior>
+            <Espacador/>
+          <ColunaMenor>
+            <TituloColuna>
+            ACORDE COM A NATUREZA À SUA PORTA
+          </TituloColuna>
+            <SubtituloColuna>
+            Todos os apartamentos se abrem diretamente para a natureza.
+Sinta o prazer de respirar ar puro.
+          </SubtituloColuna>
+          </ColunaMenor>
+        </ContainerColuna>
+      </Coluna>
+
+      <Espacador />
     </>
   )
 }
